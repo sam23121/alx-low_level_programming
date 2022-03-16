d42abafdf06c22bece2798e92e8c28b98ac2c940
Samuel Alene <smlalene@gmail.com>
2022-03-16 00:07:51 -0700

    adding 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/*
+* runs
+*
+**/
+void print_alphabet(void)
+{
+        int i;
+        for (i = 97; i < 123; i++)
+        {
+                _putchar(i);
+        }
+        _putchar('\n');
+}
