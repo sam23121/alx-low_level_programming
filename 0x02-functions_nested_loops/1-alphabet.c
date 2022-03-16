9124b8bfb6dea01eeffee15ad3c43c517ff98af9
Samuel Alene <smlalene@gmail.com>
2022-03-16 00:15:40 -0700

    adding 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
-/*
-* runs
-*
-**/
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
 void print_alphabet(void)
 {
-        int i;
-        for (i = 97; i < 123; i++)
-        {
-                _putchar(i);
-        }
-        _putchar('\n');
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
 }
